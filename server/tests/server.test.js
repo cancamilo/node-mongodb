a8dd52d588ff2b2651203b1d4e234954bfc0e6e1
camiloramirezf@hotmail.com <camiloramirezf@hotmail.com>
2016-11-18 10:25:29 -0500

    add delete route test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -108,4 +108,45 @@ describe('GET /todos/:id', () =>{
       .expect(404)
       .end(done);
   });
-})
+});
+
+describe('DELETE /todos/:id', () =>{
+  it('should remove a todo', (done) =>{
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect( (res) =>{
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) =>{
+        if(err){
+          return done(err);
+        }
+
+        Todo.findById(hexId, (res) =>{
+          expect(res).toNotExist();
+          done();
+        });
+      });
+  });
+
+  it('should retun 404 if todo not found', (done)=>{
+    // create new object id
+    var _newId = new ObjectID();
+    request(app)
+      .delete(`/todos/${_newId.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) =>{
+    var fakeId = '1243';
+    request(app)
+      .delete(`/todos/${fakeId}`)
+      .expect(404)
+      .end(done);
+  });
+
+});
